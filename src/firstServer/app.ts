eec708c7670581238e5e3e5595b4283aaa4000dd
G. Zechariah McPherson <ZechariahMcPherson@gmail.com>
2019-01-15 09:56:09 -0600

    sent post via form

--- src/firstServer/app.ts
+++ src/firstServer/app.ts
@@ -1,7 +1,18 @@
 import http from 'http';
 
 const server = http.createServer((request, response) => {
-
+    const url = request.url;
+    if(url === '/')
+    {
+        response.setHeader('Content-Type', 'text/html');
+        response.write('<html>');
+        response.write('<head><title>enter message</title></head>')
+        response.write(`<form action="/message" method="POST">
+            <input type="text" name="message">
+                <button type="submit">Send</button></form>`);
+        response.write('</html>')
+        return response.end();
+    }
     response.setHeader('Content-Type', 'text/html');
     response.write('<html>');
     response.write('<head><title>Hello from nodejs server</title></head>')
