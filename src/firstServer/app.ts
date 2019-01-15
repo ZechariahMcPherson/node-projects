fc2d85204b9c180e62614f5ef0d4cf2b7f38beec
G. Zechariah McPherson <ZechariahMcPherson@gmail.com>
2019-01-15 09:43:32 -0600

    node server sent html response

--- src/firstServer/app.ts
+++ src/firstServer/app.ts
@@ -1,5 +1,13 @@
 import http from 'http';
 
-const server = http.createServer((request, response) => console.log(request));
+const server = http.createServer((request, response) => {
+
+    response.setHeader('Content-Type', 'text/html');
+    response.write('<html>');
+    response.write('<head><title>Hello from nodejs server</title></head>')
+    response.write('<h1>Hello from nodejs server</h1>')
+    response.write('</html>')
+    response.end();
+});
 
 server.listen(3000);
