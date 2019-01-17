a94237809121fa7fcdac7cb53bfcd34ea8c07a92
G. Zechariah McPherson <ZechariahMcPherson@gmail.com>
2019-01-17 09:46:59 -0600

    set up some routing stuff for firstServer

---
+++ src/firstServer/routes.ts
@@ -0,0 +1,43 @@
+import fs from 'fs';
+import { resolve } from 'path';
+import { IncomingMessage, ServerResponse } from 'http';
+
+export const requestHandler = (
+  request: IncomingMessage,
+  response: ServerResponse,
+) => {
+  const url = request.url;
+  const method = request.method;
+  if (url === '/') {
+    response.setHeader('Content-Type', 'text/html');
+    response.write('<html>');
+    response.write('<head><title>enter message</title></head>');
+    response.write(`<form action="/message" method="POST">
+            <input type="text" name="message">
+                <button type="submit">Send</button></form>`);
+    response.write('</html>');
+    return response.end();
+  }
+
+  if (url === '/message' && method === 'POST') {
+    const body: any[] = [];
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    return request.on('end', () => {
+      const parseBody = Buffer.concat(body).toString();
+      const message = parseBody.split('=')[1];
+      fs.writeFile(resolve(__dirname, 'message.txt'), message, (error) => {
+        response.writeHead(302, { Location: '/' });
+        return response.end();
+      });
+    });
+  }
+  response.setHeader('Content-Type', 'text/html');
+  response.write('<html>');
+  response.write('<head><title>Hello from nodejs server</title></head>');
+  response.write('<h1>Hello from nodejs server</h1>');
+  response.write('</html>');
+  response.end();
+};
